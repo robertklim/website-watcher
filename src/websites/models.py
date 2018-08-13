5039e687b696f78c9e423e4a4f520392a8d93d7f
Robert Klim <kontakt.robert.klim@gmail.com>
2018-08-13 11:39:29 +0200

    Create 'Website' model

--- src/websites/models.py
+++ src/websites/models.py
@@ -1,3 +1,15 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Website(models.Model):
+    user        = models.ForeignKey(User, on_delete=models.CASCADE)
+    name        = models.CharField(max_length=128)
+    url         = models.CharField(max_length=2000)
+    description = models.CharField(max_length=1024)
+    timestamp   = models.DateField(auto_now_add=True)
+    updated     = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
