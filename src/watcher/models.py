7f7c1426eb1ac04a4652c32bc841f73fc7486e7c
Robert Klim <kontakt.robert.klim@gmail.com>
2018-08-17 12:39:35 +0200

    Create 'Check' model

--- src/watcher/models.py
+++ src/watcher/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from websites.models import Website
 
-# Create your models here.
+class Check(models.Model):
+    website         = models.ForeignKey(Website, on_delete=models.CASCADE)
+    website_hash    = models.CharField(max_length=128)
+    result          = models.CharField(max_length=32)
+    timestamp       = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.website
