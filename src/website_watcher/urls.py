832053f7d1c0ef67c881714c6e3208d7844a331a
Robert Klim <kontakt.robert.klim@gmail.com>
2018-08-20 13:01:03 +0200

    Add second 'Add website' button (menu one hides on small screens) and change home view

--- src/website_watcher/urls.py
+++ src/website_watcher/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from websites.views import WebsiteListView
 
 from .views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', HomeView.as_view(), name='home'),
+    # path('', HomeView.as_view(), name='home'),
+    path('', WebsiteListView.as_view(), name='home'),
     path('websites/', include('websites.urls', namespace='websites')),
     path('watcher/', include('watcher.urls', namespace='watcher')),
 ]
