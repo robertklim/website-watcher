f6d97373d16a4537f9534be7b0ae948dd34e05af
Robert Klim <kontakt.robert.klim@gmail.com>
2018-10-18 13:44:53 +0200

    Create and register Profile model

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    user            = models.OneToOneField(User, on_delete=models.CASCADE)
+    name            = models.CharField(max_length=32, blank=True)
+    surname         = models.CharField(max_length=32, blank=True)
+    phone_number    = models.CharField(max_length=32, blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} profile'
