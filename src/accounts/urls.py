e7c556e1750fddcf22954453a5083edd4ecb8c18
Robert Klim <kontakt.robert.klim@gmail.com>
2018-10-19 09:40:47 +0200

    Add AccountProfileView

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from .views import (
     AccountCreateView,
+    AccountProfileView,
 )
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('create/', AccountCreateView.as_view(), name='account-create'),
+    path('profile/', AccountProfileView.as_view(), name='account-profile'),
 ]
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,6 +1,10 @@
+from django.contrib.auth.models import User
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import (
     CreateView,
+    DetailView,
+    View,
 )
 
 from .forms import AccountCreateForm
@@ -11,3 +15,9 @@ class AccountCreateView(CreateView):
 
     def get_success_url(self):
         return reverse_lazy('login')
+
+class AccountProfileView(View):
+    template_name = 'accounts/account_profile.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {})
